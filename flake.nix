e50dfa866f38558e4c4b7264b95f0ef20fc70d89
Luis Sena <luis.senarode@gmail.com>
2024-07-18 20:41:56 -0300

    feat: git

--- flake.nix
+++ flake.nix
@@ -3,13 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-      ];
+
+  outputs =
+    { nixpkgs
+    , home-manager
+    , ...
+    }@inputs: {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.luis = import ./home-manager/home.nix;
+          }
+        ];
+      };
     };
-  };
 }
