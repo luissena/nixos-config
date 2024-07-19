5dcff7fe42716699bdc3f80f3bd008a033e48a62
Luis Sena <luis.senarode@gmail.com>
2024-07-19 09:51:51 -0300

    chore: remove comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,18 +1,12 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 { inputs
 , lib
 , config
 , pkgs
 , ...
 }: {
-  # You can import other home-manager modules here
+  
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
+    
   ];
 
   home = {
@@ -20,11 +14,7 @@
     homeDirectory = "/home/luis";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
+ 
   programs.home-manager.enable = true;
 
   programs.git = {
@@ -33,9 +23,7 @@
     userEmail = "luis.senarode@gmail.com";
   };
 
-  # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
